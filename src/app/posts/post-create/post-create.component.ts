011521f5ab0389ce017fc0e76057279b36e5c6fa
Byron Brown <byronanthonybrown@gmail.com>
2018-07-21 15:09:32 -0400

    before node

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-post-create',
@@ -11,20 +12,16 @@ import { Post } from '../post.model';
 export class PostCreateComponent implements OnInit {
   enteredTitle = '';
   enteredContent = '';
-  @Output() postCreated = new EventEmitter<Post>();
 
-  constructor() { }
+  constructor(private postsService: PostsService) { }
 
   ngOnInit() {
   }
 
   onAddPost(form: NgForm) {
     if (form.invalid) { return };
-    const post: Post = {
-      title: form.value.title,
-      content: form.value.content    
-    }
-    this.postCreated.emit(post);
+    this.postsService.addPost(form.value.title, form.value.content)
+    form.resetForm();
   }
 
 }
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,5 +1,24 @@
 import { Post } from './post.model';
+import { Subject } from 'rxjs';
 
-export class PostService {
-    posts: Post[] = [];
+export class PostsService {
+    private posts: Post[] = [];
+    private postsUpdated = new Subject<Post[]>();
+
+    getPosts() {
+        return [...this.posts];
+    }
+
+    getPostsUpdatedListener() {
+        return this.postsUpdated.asObservable();
+    }
+
+    addPost(title: string, content: string) {
+        const post: Post = {
+            title,
+            content
+        }
+        this.posts.push(post);
+        this.postsUpdated.next([...this.posts]);
+    }
 }
